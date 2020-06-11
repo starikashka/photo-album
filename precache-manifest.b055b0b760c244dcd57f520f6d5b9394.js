04e31e867738fdc4ee2a4a64f31023186f531a0f
starikashka <starikashka@gmail.com>
2020-06-11 12:14:54 +0300

    Updates

---
+++ precache-manifest.b055b0b760c244dcd57f520f6d5b9394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a9b15445764b5680e8ef94ff7bf867",
+    "url": "https://starikashka.github.io/photo-album/ /index.html"
+  },
+  {
+    "revision": "205719812734e4d422da",
+    "url": "https://starikashka.github.io/photo-album/ /static/css/main.3732b338.chunk.css"
+  },
+  {
+    "revision": "619573564b09f8f07722",
+    "url": "https://starikashka.github.io/photo-album/ /static/js/2.fe899c6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://starikashka.github.io/photo-album/ /static/js/2.fe899c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205719812734e4d422da",
+    "url": "https://starikashka.github.io/photo-album/ /static/js/main.d41e8495.chunk.js"
+  },
+  {
+    "revision": "a70e979983dcbe3e6154",
+    "url": "https://starikashka.github.io/photo-album/ /static/js/runtime-main.5c54a1ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/photo-album/precache-manifest.0368de42d718cbfb2a6ae8dec0ddc33c.js"
+  "https://starikashka.github.io/photo-album/ /precache-manifest.b055b0b760c244dcd57f520f6d5b9394.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/photo-album/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://starikashka.github.io/photo-album/ /index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
